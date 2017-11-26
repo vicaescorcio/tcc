d6b499a6a22e87ba8212ea3540580e2ed3ed2e24
Vicente Lucas <vica.escorcio@gmail.com>
2017-11-26 17:41:49 -0300

    favicon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_member!
   layout 'layouts/application'
   def index
-    @ideas = Idea.order(:cached_votes_up =>:desc)
+    @ideas = Idea.all
   end
 end
