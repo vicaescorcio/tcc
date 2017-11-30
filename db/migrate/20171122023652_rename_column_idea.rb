418d7ce717a0b30aa62d99351ad2f156a7ed59db
Vicente Lucas <vica.escorcio@gmail.com>
2017-11-29 22:33:30 -0300

    databse

--- db/migrate/20171122023652_rename_column_idea.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumnIdea < ActiveRecord::Migration[5.1]
-  def self.up
-    rename_column :ideas, :idea_id, :id
-  end
-  
-  def self.down
-    rename_column :ideas, :id, :idea_id
-  end
-end
