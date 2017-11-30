19cef2f83c4e6ea074f8a299ac0b74b2d30e0c2d
Vicente Lucas <vica.escorcio@gmail.com>
2017-11-29 23:10:58 -0300

    database

--- db/migrate/20171122185122_change_column_ideas.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnIdeas < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :ideas, :type, :kind
-  end
-end
--- db/migrate/20171122205211_rename_column_type_idea.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnTypeIdea < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :ideas, :type, :kind
-  end
-end
